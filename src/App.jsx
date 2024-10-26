ce15560af693ebce715a4f9caf4ed51ab0af4678
Ben-Arlanda <ben.arlanda@protonmail.com>
2024-10-26 14:28:30 +1100

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Card } from "./components/card";
 
 function App() {
-  const [activeCard, setActiveCard] = useState(1)
+  const [activeCard, setActiveCard] = useState(1);
 
 const cards = [
   {id: 0,
@@ -23,45 +23,50 @@ const cards = [
   },
 ]
 
-  const handleClick = (id) => {
-    setActiveCard(id)
-  }
+const handleClick = (id) => {
+  setActiveCard(id)
+}
 
-  return (
-    <>
+return (
+  <>
     <section>
-        <div className="relative">
-          <img src="/ford-ranger_rd5m4t.jpg" alt="Ford Ranger" className="w-full md:h-auto shadow-lg" loading="lazy" />
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="absolute inset-1 flex flex-col justify-center items-center text-center text-gray-200">
+      <div className="relative">
+        <img
+          src="/ford-ranger_rd5m4t.jpg"
+          alt="Ford Ranger"
+          className="w-full md:h-auto shadow-lg"
+          loading="lazy"
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-1 flex flex-col justify-center items-center text-center text-gray-200">
           <h1 className="md:text-3xl text-xs font-bold md:mb-4 mt-2 transition-all duration-300 ease-in-out mb-1">
-            Welcome to G Automotive
+          Welcome to G Automotive
           </h1>
           <p className="max-w-lg md:mb-5 mb-2 md:text-xs text-xxs w-auto h-auto overflow-hidden mr-6 ml-6">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto illo eos unde molestiae voluptas quaerat totam ducimus amet cumque delectus qui sed iste, reprehenderit ratione magni necessitatibus sunt.
             Recusandae, adipisci! Lorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing.
           </p>
           <button className="bg-indigo-500 rounded-2xl md:text-xs text-xxs pr-7 pl-7 pt-1.5 pb-1.5 font-semibold mb-3 transition-transform transform hover:scale-105">
-            Contact Us
+          Contact Us
           </button>
-          </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section>
-        <div className="flex md:justify-center items-center py-6 space-x-7 md:mt-4 mr-4 ml-4">
-          {cards.map((card) => (
-            <Card
-              key={card.id}
-              card={card}
-              isActive={activeCard === card.id}
-              onClick={() => handleClick(card.id)}
-            />
-          ))}
-        </div>
-      </section>
-    </>
+    <section>
+      <div className="flex md:justify-center items-center py-6 space-x-7 md:mt-4 mr-4 ml-4">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            isActive={activeCard === card.id}
+            onClick={() => handleClick(card.id)}
+          />
+        ))}
+      </div>
+    </section>
+  </>
   )
 }
 
-export default App
+export default App;
